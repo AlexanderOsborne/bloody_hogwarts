925c5d233f61bfd4c7f221bf0d3570a792b11078
awosborn <alanturing@example.coclear>
2020-12-22 12:14:42 -0700

    Add story two test

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -24,5 +24,19 @@ RSpec.describe 'As a visitor', type: :feature do
       expect(page).to have_content(harry.house)
 
     end
+    it 'I see a list of students with name, age and house' do
+      harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+
+      dod = Course.create(name:"Defense of the dark arts")
+      potions = Course.create(name:"Potions")
+
+      StudentCourse.create(student_id: harry.id, course_id: dod.id)
+      StudentCourse.create(student_id: harry.id, course_id: potions.id)
+
+      visit '/students/#{harry.id}'
+
+      expect(page).to have_content(harry.name)
+      expect(page).to have_content(harry.courses)
+    end
   end
 end
