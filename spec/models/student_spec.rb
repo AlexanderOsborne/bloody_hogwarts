a94fc377629f872680e908e7e2af3854e17f2aca
awosborn <alanturing@example.coclear>
2020-12-22 13:00:49 -0700

    Fix self.alphabetize test

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -13,19 +13,20 @@ RSpec.describe Student, type: :model do
     it {should have_many(:courses).through(:student_courses)}
   end
 
-  describe 'class methods' do
+  describe 'instance methods' do
+    it 'self.alphabetize' do 
     harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
-      malfoy = Student.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
+    malfoy = Student.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
 
-      dod = Course.create(name:"Defense of the dark arts")
-      potions = Course.create(name:"Potions")
+    dod = Course.create(name:"Defense of the dark arts")
+    potions = Course.create(name:"Potions")
 
-      StudentCourse.create(student_id: harry.id, course_id: dod.id)
-      StudentCourse.create(student_id: harry.id, course_id: potions.id)
-      StudentCourse.create(student_id: malfoy.id, course_id: potions.id)
+    StudentCourse.create(student_id: harry.id, course_id: dod.id)
+    StudentCourse.create(student_id: harry.id, course_id: potions.id)
+    StudentCourse.create(student_id: malfoy.id, course_id: potions.id)
 
-      dod.students_per_course == 1
-      potions.students_per_course == 2
+    expect(dod.students_per_course).to eq(1)
+    expect(potions.students_per_course).to eq(2)
+    end
   end
-
 end
